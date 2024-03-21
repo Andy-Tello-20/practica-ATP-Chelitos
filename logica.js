528c9ed98a671dc225cb88352a60a8afe8455764
Andrés Tello <andrest911@gmail.com>
2024-03-20 21:37:16 -0300

    logica

--- logica.js
+++ logica.js
@@ -1,9 +1,7 @@
 
 const btnCart = document.getElementsByClassName('img-usuario')[0]
 
-const containerCartProducts = document.querySelector(
-	'.container-cart-products'
-);
+const containerCartProducts = document.getElementsByClassName('user-options')[0];
 
 btnCart.addEventListener('click', () => {
 	containerCartProducts.classList.toggle('hidden-cart');
