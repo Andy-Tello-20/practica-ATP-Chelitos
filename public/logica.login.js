bf16426e350119d3c6c1a25e991659fc93dfb45b
Andrés Tello <andrest911@gmail.com>
2024-03-22 10:13:49 -0300

    bordes rojos inputs error

--- public/logica.login.js
+++ public/logica.login.js
@@ -3,12 +3,23 @@ const emailInput = document.getElementById('floatingInput')
 const passwordInput = document.getElementById('floatingPassword')
 const errorMessage = document.getElementById('errorMessage')
 
+
+
+    emailInput.style.border = ''
+    passwordInput.style.border = ''
+
+
 btnAcceder.addEventListener('click', (e) => {
     const email = emailInput.value
     const password = passwordInput.value
 
     if (email !== 'atello' || password !== '123456') {
         errorMessage.textContent = 'Por favor revise su usuario y/o contraseña e intente nuevamente'
+
+
+        emailInput.style.border = '1px solid red'
+        passwordInput.style.border = '1px solid red'
+
         e.preventDefault()
     }
 })
