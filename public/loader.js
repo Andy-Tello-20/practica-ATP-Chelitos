b3742812f44bac7d122c32b97ff40a89c2ad70bc
Andrés Tello <andrest911@gmail.com>
2024-03-25 22:07:25 -0300

    loader

---
+++ public/loader.js
@@ -0,0 +1,20 @@
+const preloaderWrapper = document.querySelector('.preloader-wrapper');
+
+let esperar = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        preloaderWrapper.classList.add('fade-out-animation');
+      
+      resolve(); // Resolvemos la promesa para indicar que ha terminado el setTimeout
+    }, 4000);
+  });
+};
+
+window.addEventListener('load', async () => {
+  await esperar(); // Esperamos a que termine el setTimeout antes de continuar
+
+  setTimeout(() => {
+    preloaderWrapper.style.display = 'none';
+  }, 1000);
+  
+});
