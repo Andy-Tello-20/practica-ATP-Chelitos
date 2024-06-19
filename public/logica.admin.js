75821f651558a273c340e360fe4ef9ba9512d9f3
Andrés Tello <andrest911@gmail.com>
2024-06-18 21:08:11 -0300

    nuevos cambios en el modal

--- public/logica.admin.js
+++ public/logica.admin.js
@@ -49,157 +49,212 @@ document.addEventListener('DOMContentLoaded', function () {
 
 //?   Logica botones X
 
-
-
 let originalValues = {};
-
 
 document.querySelectorAll('.btn-edit').forEach(button => {
     button.addEventListener('click', function () {
-        const suffix = this.getAttribute('data-button')
+        const suffix = this.getAttribute('data-button');
 
-
-        const editableElements = document.querySelectorAll(`.editable-${suffix}`)
-        let pTitle = document.querySelectorAll(`.dto-title-${suffix}`)
-        let labels = document.querySelectorAll(`.active-label-${suffix}`)
-        let mIcons = document.querySelectorAll(`.mIcons-${suffix}`)
-
+        const editableElements = document.querySelectorAll(`.editable-${suffix}, .editable-select-${suffix} ,.editable-select-prof-${suffix}`);
+        let pTitle = document.querySelectorAll(`.dto-title-${suffix}`);
+        let labels = document.querySelectorAll(`.active-label-${suffix}`);
+        let mIcons = document.querySelectorAll(`.mIcons-${suffix}`);
 
         mIcons.forEach(i => {
-            i.classList.add('class-hidden-none')
-        })
-
-        pTitle.forEach(element => {
-            element.classList.add('class-hidden-none')
-        })
-
-        labels.forEach(l => {
-            l.classList.remove('class-hidden-none')
-        })
-
-
-        originalValues[suffix] = {}
-        editableElements.forEach(element => {
-            originalValues[suffix][element.id] = element.textContent
-
-            const input = document.createElement('input')
-            input.type = 'text'
-            input.value = element.textContent
-            input.className = `editable-input-${suffix} form-control`
-            input.setAttribute('data-id', element.id)
-
-
-            element.replaceWith(input);
+            i.classList.add('class-hidden-none');
         });
 
+        pTitle.forEach(element => {
+            element.classList.add('class-hidden-none');
+        });
 
-        document.querySelector(`.sc-buttons-${suffix}`).style.display = 'flex'
+        labels.forEach(l => {
+            l.classList.remove('class-hidden-none');
+        });
+
+        originalValues[suffix] = {};
+        editableElements.forEach(element => {
+            originalValues[suffix][element.id] = element.textContent;
+
+            if (element.classList.contains(`editable-select-${suffix}`)) {
 
 
-        button.disabled = true
+                const select = document.createElement('select');
+                select.className = `editable-select-${suffix} form-control`;
+                select.setAttribute('data-id', element.id);
+                const option = document.createElement('option');
+                option.value = element.textContent;
+                option.textContent = element.textContent;
+                select.appendChild(option);
+
+
+                ['DNI', 'Pasaporte', 'UE'].forEach(optionText => {
+                    const option = document.createElement('option');
+                    option.value = optionText;
+                    option.textContent = optionText;
+                    if (optionText === element.textContent) {
+                        option.selected = true;
+                    }
+                    select.appendChild(option);
+                });
+                element.replaceWith(select);
+
+
+            } else if (element.classList.contains(`editable-select-prof-${suffix}`)) {
+
+                console.log('hola')
+                const select = document.createElement('select');
+                select.className = `editable-select-prof-${suffix} form-control `;
+                select.setAttribute('data-id', element.id);
+                const option = document.createElement('option');
+                option.value = element.textContent;
+                option.textContent = element.textContent;
+                select.appendChild(option);
+
+
+                ['Ingeniero civil', 'Diseñador gráfico', 'Economista financiero', 'Abogado de derechos humanos', 'Traductor/Intérprete de conferencias'].forEach(optionText => {
+                    const option = document.createElement('option');
+                    option.value = optionText;
+                    option.textContent = optionText;
+                    if (optionText === element.textContent) {
+                        option.selected = true;
+                    }
+                    select.appendChild(option);
+                });
+                element.replaceWith(select);
+
+
+
+
+            } else {
+                const input = document.createElement('input');
+                input.type = 'text';
+                input.value = element.textContent;
+                input.className = `editable-input-${suffix} form-control`;
+                input.setAttribute('data-id', element.id);
+
+
+
+                element.replaceWith(input);
+            }
+        });
+
+        document.querySelector(`.sc-buttons-${suffix}`).style.display = 'flex';
+        button.disabled = true;
     });
 });
 
-
 document.querySelectorAll('.btn-save').forEach(button => {
     button.addEventListener('click', function () {
+        const suffix = this.getAttribute('data-save');
 
+        let pTitle = document.querySelectorAll(`.dto-title-${suffix}`);
+        let labels = document.querySelectorAll(`.active-label-${suffix}`);
+        let mIcons = document.querySelectorAll(`.mIcons-${suffix}`);
 
-        const suffix = this.getAttribute('data-save')
-
-        console.log('suffix es: ', suffix)
-
-        let pTitle = document.querySelectorAll(`.dto-title-${suffix}`)
-        let labels = document.querySelectorAll(`.active-label-${suffix}`)
-        let mIcons = document.querySelectorAll(`.mIcons-${suffix}`)
-        // Restaurar los campos a elementos span
-        const inputs = document.querySelectorAll(`.editable-input-${suffix}`)
+        const inputs = document.querySelectorAll(`.editable-input-${suffix}`);
         inputs.forEach(input => {
-            const span = document.createElement('span')
-            span.className = `editable-${suffix}`
-            span.textContent = input.value
-            span.id = input.getAttribute('data-id')
+            const span = document.createElement('span');
+            span.className = `editable-${suffix}`;
+            span.textContent = input.value;
+            span.id = input.getAttribute('data-id');
 
+            input.replaceWith(span);
+        });
 
-            input.replaceWith(span)
+        const selects = document.querySelectorAll(`.editable-select-${suffix}`);
+        selects.forEach(select => {
+            const span = document.createElement('span');
+            span.className = `editable-select-${suffix}`;
+            span.textContent = select.value;
+            span.id = select.getAttribute('data-id');
+
+            select.replaceWith(span);
         });
 
 
+        const selectP = document.querySelectorAll(`.editable-select-prof-${suffix}`);
+        selectP.forEach(select => {
+            const span = document.createElement('span');
+            span.className = `editable-select-prof-${suffix}`;
+            span.textContent = select.value;
+            span.id = select.getAttribute('data-id');
 
-
+            select.replaceWith(span);
+        });
 
 
         mIcons.forEach(i => {
-            i.classList.remove('class-hidden-none')
-        })
-
+            i.classList.remove('class-hidden-none');
+        });
 
         pTitle.forEach(element => {
-            element.classList.remove('class-hidden-none')
+            element.classList.remove('class-hidden-none');
         });
 
         labels.forEach(l => {
-            l.classList.add('class-hidden-none')
+            l.classList.add('class-hidden-none');
         });
 
-
-        document.querySelector(`.sc-buttons-${suffix}`).style.display = 'none'
-
-
-        document.getElementById(`editButton-${suffix}`).disabled = false;
-
-    })
-})
-
+        document.querySelector(`.sc-buttons-${suffix}`).style.display = 'none';
+        document.querySelector(`.btn-edit[data-button="${suffix}"]`).disabled = false;
+    });
+});
 
 document.querySelectorAll('.btn-cancel').forEach(button => {
     button.addEventListener('click', function () {
+        const suffix = this.getAttribute('data-cancel');
 
-        const suffix = this.getAttribute('data-cancel')
+        let pTitle = document.querySelectorAll(`.dto-title-${suffix}`);
+        let labels = document.querySelectorAll(`.active-label-${suffix}`);
+        let mIcons = document.querySelectorAll(`.mIcons-${suffix}`);
 
-        let pTitle = document.querySelectorAll(`.dto-title-${suffix}`)
-        let labels = document.querySelectorAll(`.active-label-${suffix}`)
-        let mIcons = document.querySelectorAll(`.mIcons-${suffix}`)
-
-        const inputs = document.querySelectorAll(`.editable-input-${suffix}`)
+        const inputs = document.querySelectorAll(`.editable-input-${suffix}`);
         inputs.forEach(input => {
-            const span = document.createElement('span')
-            span.className = `editable-${suffix}`
-            span.textContent = originalValues[suffix][input.getAttribute('data-id')]
-            span.id = input.getAttribute('data-id')
+            const span = document.createElement('span');
+            span.className = `editable-${suffix}`;
+            span.textContent = originalValues[suffix][input.getAttribute('data-id')];
+            span.id = input.getAttribute('data-id');
 
+            input.replaceWith(span);
+        });
 
-            input.replaceWith(span)
+        const selects = document.querySelectorAll(`.editable-select-${suffix}`);
+        selects.forEach(select => {
+            const span = document.createElement('span');
+            span.className = `editable-select-${suffix}`;
+            span.textContent = originalValues[suffix][select.getAttribute('data-id')];
+            span.id = select.getAttribute('data-id');
+
+            select.replaceWith(span);
         });
 
 
+        const selectP = document.querySelectorAll(`.editable-select-prof-${suffix}`);
+        selectP.forEach(select => {
+            const span = document.createElement('span');
+            span.className = `editable-select-prof-${suffix}`;
+            span.textContent = select.value;
+            span.id = select.getAttribute('data-id');
 
-
-
+            select.replaceWith(span);
+        });
 
         mIcons.forEach(i => {
-            i.classList.remove('class-hidden-none')
-        })
+            i.classList.remove('class-hidden-none');
+        });
 
         pTitle.forEach(element => {
-            element.classList.remove('class-hidden-none')
-        })
+            element.classList.remove('class-hidden-none');
+        });
 
         labels.forEach(l => {
-            l.classList.add('class-hidden-none')
-        })
+            l.classList.add('class-hidden-none');
+        });
 
-
-        document.querySelector(`.sc-buttons-${suffix}`).style.display = 'none'
-
-
-        document.getElementById(`editButton-${suffix}`).disabled = false
-
-
+        document.querySelector(`.sc-buttons-${suffix}`).style.display = 'none';
+        document.querySelector(`.btn-edit[data-button="${suffix}"]`).disabled = false;
     });
-
-})
-
-
+});
 
 
